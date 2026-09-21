2c24e4f352c4be4b448b4b0e8ab99fdc95cd1f47
turattiss <isabelaturattis@gmail.com>
2026-09-21 10:25:41 -0300

    Inicialização

---
+++ src/data/portfolio.js
@@ -0,0 +1,38 @@
+import imgVictor from '../assets/imgs/vctr.jpg'
+import imgMethos from '../assets/imgs/methos.jpg'
+import imgVerassi from '../assets/imgs/verassi.jpg'
+import imgAlianca from '../assets/imgs/alianca.jpg'
+import imgMarca from '../assets/imgs/marca.jpg'
+
+export const projetos = [
+  {
+    nome: 'Victor',
+    ano: 2024,
+    imagem: imgVictor,
+  },
+  {
+    nome: 'Victor',
+    ano: 2014,
+    imagem: imgVictor,
+  },
+  {
+    nome: 'Methos',
+    ano: 2023,
+    imagem: imgMethos,
+  },
+  {
+    nome: 'Verassi',
+    ano: 2024,
+    imagem: imgVerassi,
+  },
+  {
+    nome: 'Aliança',
+    ano: 2022,
+    imagem: imgAlianca,
+  },
+  {
+    nome: 'Marca',
+    ano: 2025,
+    imagem: imgMarca,
+  },
+]
